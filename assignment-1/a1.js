e826667c687034b71149c455acf221bd891b1b81
Rafael Tedin Alvarez <rtedin@gmail.com>
2015-07-16 17:57:57 +0300

    Add rotation

--- assignment-1/a1.js
+++ assignment-1/a1.js
@@ -17,6 +17,27 @@ function Triangle(a, b, c) {
 }
 
 /**
+ * Returns the vertex resulting form rotating vertex 'vertex' 'theta' radians
+ * around the origin.
+ */
+function rotateVertex(vertex, theta) {
+   var x = vertex[0];
+   var y = vertex[1];
+   sintheta = Math.sin(theta);
+   costheta = Math.cos(theta);
+   return vec2(x * costheta - y * sintheta, x * sintheta + y * costheta);
+}
+
+/**
+ * Rotates the vertices 'vertices' by 'theta' radians around the origin.
+ */
+function rotateVertices(vertices, theta) {
+   for(var i = 0; i < vertices.length; i++) {
+      vertices[i] = rotateVertex(vertices[i], theta);
+   }
+}
+
+/**
  * Divides a triangle 'triangle' recursively a 'numDiv' of times and stores the
  * vertices in 'vertices'.
  */
@@ -30,6 +51,7 @@ function divideTriangle(triangle, numDiv, vertices) {
       divideTriangle(Triangle(triangle.a, ab, ac), numDiv - 1, vertices);
       divideTriangle(Triangle(ab, triangle.b, bc), numDiv - 1, vertices);
       divideTriangle(Triangle(ac, bc, triangle.c), numDiv - 1, vertices);
+//       divideTriangle(Triangle(ab, bc, ac), numDiv - 1, vertices);
    }
 }
 
@@ -38,9 +60,12 @@ function divideTriangle(triangle, numDiv, vertices) {
  * 'numDiv' is the number of times to divide the original triangle.
  */
 function createVerticesArray(numDiv) {
-   var firstTriangle = Triangle(vec2(-1, -1), vec2(0, 1), vec2(1, -1));
+   var a = vec2(0, 0.7);
+   var triangle = Triangle(a, rotateVertex(a, 2.0 * Math.PI / 3.0),
+                           rotateVertex(a, -2.0 * Math.PI / 3.0));
    var vertices = [];
-   divideTriangle(firstTriangle, numDiv, vertices);
+   divideTriangle(triangle, numDiv, vertices);
+   rotateVertices(vertices, Math.PI/2);
    return vertices;
 }
 
@@ -71,7 +96,7 @@ function render(gl, numVertices) {
 
 window.onload = function init() {
    var gl = setUpCanvas(); if(!gl) { alert("WegGL is not available"); }
-   var vertices = createVerticesArray(5);
+   var vertices = createVerticesArray(4);
    sendVerticesToGPU(vertices, gl);
    render(gl, vertices.length);
 }
